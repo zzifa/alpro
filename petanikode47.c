1c04b02825fb8622d9a0ed0032c6cd52e6c4d834
zzifa <100108842+zzifa@users.noreply.github.com>
2023-10-08 23:09:59 +0700

    Update petanikode47.c

--- petanikode47.c
+++ petanikode47.c
@@ -1,23 +1,17 @@
 #include <stdio.h>
 
-// comment
-
-int main () {
-    char nama[20], kelas[20], jurusan[20];
+int main(){
+    char name[50], email[50];
     
     printf("Nama: ");
-    fgets(nama, sizeof(nama), stdin);
-
-    printf("Kelas: ");
-    fgets(kelas, sizeof(kelas), stdin);
+    fgets(name, sizeof(name), stdin);
     
-    printf("Jurusan: ");
-    fgets(jurusan, sizeof(jurusan), stdin);
-
-    printf("\n=========\n");
-    printf("Nama : %s", nama);
-    printf("Kelas : %s", kelas);
-    printf("Jurusan: %s\n", jurusan);
+    printf("Email: ");
+    fgets(email, sizeof(email), stdin);
    
+    printf("\n-------------------------\n");
+    printf("Nama anda: %s", name);
+    printf("Alamat email: %s", email);
+    
     return 0;
 }
