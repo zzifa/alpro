7bd512d7e5149f09b5bcafca3f72d99981fd77be
Azifah Rahimah Shoerya <azifahrahimah10@gmail.com>
2023-10-18 15:47:39 +0700

    terakhir

---
+++ tugas02-petanikode/petanikode65.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(){
+    int a = 4;
+    int b = 3;
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+    // operator pembanding
+    printf("a > b = %d\n", a > b);
+    printf("a < b = %d\n", a < b);
+    printf("a >= b = %d\n", a >= b);
+    printf("a <= b = %d\n", a <= b);
+    printf("a == b = %d\n", a == b);
+    printf("a != b = %d\n", a != b);
+}
+// nilai true=1, false=0
