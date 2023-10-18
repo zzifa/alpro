55fb63096489da7e9a9a4bbefca94b878ce239a9
zzifa <100108842+zzifa@users.noreply.github.com>
2023-10-18 15:51:14 +0700

    Update BMI.c

--- BMI.c
+++ BMI.c
@@ -9,15 +9,21 @@ printf("\n1. Pria");
 printf("\n2. Wanita\n");
 scanf("%d", &x);
 
-switch(x){
-    case 1:
     printf("\nmasukkan tinggi badan (m) : ");
     scanf("%f", &tinggi);
     printf("masukkan berat badan (kg) : ");
     scanf("%f", &berat);
 
+switch(x){
+    case 1:
+    // printf("\nmasukkan tinggi badan (m) : ");
+    // scanf("%f", &tinggi);
+    // printf("masukkan berat badan (kg) : ");
+    // scanf("%f", &berat);
+
     BMI = berat/(tinggi*tinggi);
 
+    printf("BMI = %.2f\n", BMI);
     if(BMI>=17 && BMI<=23){
         printf("Berat Badan Ideal Untuk Pria");
     }
@@ -25,13 +31,14 @@ switch(x){
     break;
 
     case 2:
-    printf("\nmasukkan tinggi badan (m) : ");
-    scanf("%f", &tinggi);
-    printf("masukkan berat badan (kg) : ");
-    scanf("%f", &berat);
+    // printf("\nmasukkan tinggi badan (m) : ");
+    // scanf("%f", &tinggi);
+    // printf("masukkan berat badan (kg) : ");
+    // scanf("%f", &berat);
 
     BMI = berat/(tinggi*tinggi);
 
+    printf("BMI = %.2f\n", BMI);
     if(BMI>=18 && BMI<=25){
         printf("Berat Badan Ideal untuk Wanita");
     }
