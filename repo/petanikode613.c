8ef53236f954b147d51244b0f98b55dabc0375eb
Azifah Rahimah Shoerya <azifahrahimah10@gmail.com>
2023-10-23 21:03:58 +0700

    cleaning up

--- repo/petanikode613.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    // membuat pointer
-    int *a = 5;
-
-    // ambil alamat memori dari pointer a
-    printf("alama memori variabel a adalah %x\n", &a);
-}
--- repo/petanikode68.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    int a = 6;
-    int b = 3;
-
-    printf("a | b = %d\n", a | b);
-}
