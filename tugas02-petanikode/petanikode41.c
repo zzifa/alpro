9c31da50f6587612bd69116dd7b31c2b0c814ebb
Azifah Rahimah Shoerya <azifahrahimah10@gmail.com>
2023-10-23 20:53:22 +0700

    rename folder

---
+++ tugas02-petanikode/petanikode41.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    printf("Halo, ini adalah teks output\n");
+    printf("Nama saya %s\n", "Azifah");
+    printf("Usia saya %d\n", 18);
+    return 0;
+}
---
+++ tugas02-petanikode/petanikode615.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void main(){
+    int a = 4;
+
+    // increment a
+    a++;
+
+    printf("a++ = %d\n", a);
+
+    // increment a
+    ++a;
+
+    printf("++a = %d\n", a);
+
+    // decrement a
+    a--;
+
+    printf("a-- = %d\n", a);
+
+    //increment dan decrment untuk tambah atau kurang 1
+
+    // decrement a
+    --a;
+
+    printf("--a = %d\n", a);
+}
