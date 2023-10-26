85fb0787ef2a00d94c3923492a795a62c70c289e
Azifah Rahimah Shoerya <azifahrahimah10@gmail.com>
2023-10-26 19:01:51 +0700

    clean

--- Tugas05-Petani/array1.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    char huruf[5] = {'a', 'b', 'c', 'd', 'e'};
-
-    printf("Huruf: %c\n", huruf[2]);
-}
--- Tugas05-Petani/loopfor2.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void main(){
-    for(int counter = 0; counter < 50; counter+=2){
-        printf("Perulangan ke-%i\n", counter);
-    }
-}
